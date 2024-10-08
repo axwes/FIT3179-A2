bdb8e5dea6b049665a5c16acf5f2fb478eb8b444
ytan0255 <ytan0255@student.monash.edu>
2024-10-08 20:45:39 +1100

    Sorted files into folders and added new CSV for travelling reasons

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,5 +1,5 @@
-var vg_1 = "australia-visitor-country.vg.json";
-var vg_2 = "australia-total-visitors.vg.json";
+var vg_1 = "Country_Origin/australia-visitor-country.vg.json";
+var vg_2 = "Each_State_Visitors/australia-total-visitors.vg.json";
 
 vegaEmbed("#country_origin_chart", vg_1).then(function(result) {
     // Access the Vega view instance as result.view
