d8af74729c822eb31b3c89a6c44ca69e308e782b
ytan0255 <ytan0255@student.monash.edu>
2024-10-09 05:14:12 +1100

    added new csv

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,10 +1,15 @@
 var vg_1 = "Country_Origin/australia-visitor-country.vg.json";
 var vg_2 = "Each_State_Visitors/australia-total-visitors.vg.json";
+var vg_3 = "Travelling_Reasons/travelling-reasons.vg.json";
 
 vegaEmbed("#country_origin_chart", vg_1).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 vegaEmbed("#total_visitor_chart", vg_2).then(function(result) {
+    // Access the Vega view instance as result.view
+}).catch(console.error);
+
+vegaEmbed("#travel_reasons", vg_3).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
