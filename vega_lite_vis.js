14ca75d6282a276ef236bd1fe0eb7ffe5e9bf3fe
ytan0255 <ytan0255@student.monash.edu>
2024-10-10 11:31:32 +1100

    changed style of html

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,6 +1,7 @@
 var vg_1 = "Country_Origin/australia-visitor-country.vg.json";
 var vg_2 = "Each_State_Visitors/australia-total-visitors.vg.json";
 var vg_3 = "Travelling_Reasons/travelling-reasons.vg.json";
+var vg_4 = "What_To_Do/activities.vg.json";
 
 vegaEmbed("#country_origin_chart", vg_1).then(function(result) {
     // Access the Vega view instance as result.view
@@ -11,5 +12,9 @@ vegaEmbed("#total_visitor_chart", vg_2).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#travel_reasons", vg_3).then(function(result) {
+    // Access the Vega view instance as result.view
+}).catch(console.error);
+
+vegaEmbed("#activities", vg_4).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
