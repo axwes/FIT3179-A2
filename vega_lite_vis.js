a0bb852ef90a838495c6c014e7e20f40c7b827e9
ytan0255 <ytan0255@student.monash.edu>
2024-10-15 19:04:01 +1100

    finalised design for dv2

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,6 +2,7 @@ var vg_1 = "Country_Origin/australia-visitor-country.vg.json";
 var vg_2 = "Each_State_Visitors/australia-total-visitors.vg.json";
 var vg_3 = "Travelling_Reasons/travelling-reasons.vg.json";
 var vg_4 = "What_To_Do/activities.vg.json";
+var vg_5 = "Spending/spending.vg.json";
 
 vegaEmbed("#country_origin_chart", vg_1).then(function(result) {
     // Access the Vega view instance as result.view
@@ -16,5 +17,9 @@ vegaEmbed("#travel_reasons", vg_3).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#activities", vg_4).then(function(result) {
+    // Access the Vega view instance as result.view
+}).catch(console.error);
+
+vegaEmbed("#spending", vg_5).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
